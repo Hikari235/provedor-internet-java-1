95be4fa2aed52ef0962bd193b5ffe8b764bf4ee4
Pedro Giovani Mussini <mussinipedro12235@gmail.com>
2024-04-08 19:56:55 -0300

    resolução de problema na entrada de texto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String provedor = "CineMax";
+        System.out.printf("Olá, bem-vindo a central de atendimento %s", provedor);
+        String atendente = "Mary Jane";
+        System.out.printf("\nMeu nome é %s, sou seu assistente virtual", atendente);
+
+        System.out.print("\nDurante o atendimento siga minhas instruções");
+
+        System.out.print("\nPrimeiramente informe seu nome");
+        Scanner scanner = new Scanner(System.in);
+        String nome = scanner.nextLine();
+
+        System.out.printf("Obrigado %s, como posso lhe ajudar?", nome);
+
+        String resposta = scanner.nextLine();
+
+        System.out.printf("\nSua resposta foi: \n %s", resposta);
+        System.out.println("\nSua resposta foi:" + resposta);
+        scanner.close();
     }
 }
